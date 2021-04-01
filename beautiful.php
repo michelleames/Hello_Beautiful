c601ab8035821f913018e22a3ba50541e244a0e2
Ismail Siraje Ittembe <33895119+ismailsirajeittembe@users.noreply.github.com>
2021-04-01 16:00:41 +0300

    1-04-2021

--- beautiful.php
+++ beautiful.php
@@ -1,33 +1,33 @@
 <?php
 /**
- * @package Hello_Beautiful
- * @version 1.1
+ * @package Hello_quran
+ * @version 20.21
  */
 /*
-Plugin Name: Hello Beautiful
-Plugin URI: https://worksbymichelle.com/hello-beautiful
-Description: This is not just a plugin, it tells you how amazing you are! <cite>Hello, Beautiful</cite> in the upper right of your admin screen on every page.
-Author: Michelle Frechette
-Version: 1.1
-Author URI: https://worksbymichelle.com/
+Plugin Name: Hello Quran
+Plugin URI: https://ismailsirajeittembe.com/plugins/hello-quran
+Description: Assalam Alaikum to you all, on 29 March 2021 I forked HelloBeautiful, a one PHP file plugin by Michelle Frechette who works for https://impress.org the guy behind GiveWP WordPress Donation and WP Business Reviews Plugins. <br>She has a very nice post worth reading here https://impress.org/how-hello-dolly-helped-me-appreciate-developers-even-more/ which got me into this whole thing of creating my own WordPress Plugin. Since PHP doesn’t scare me <br>Like all Muslims around the world, we are constantly goingthrough a journey of self-discovery and personal improvement through theguidance of Allah's words.<br>This is not just a plugin, it's a comprehensive list of TheHoly Quran verses as quotes that teach us ways to improve our life.Guidance regarding every aspect of life. Remember Reading the Qur'an helps inrelieving stress and reducing anxiety and depression. <br>So, I hope this effort helps bring us closer to Allah andThe Holy Quran in the end Amen. When activated you will randomly see a quotefrom The Holy Quran, in the upper right of your admin screen on every page. <br>Read about me coding it on my bloghttps://ismailsirajeittembe.com/hello-quran-plugin/.
+Author: Ismail Siraje Ittembe
+Version: 20.21
+Author URI: https://ismailsirajeittembe.com/
 */
 
-function hello_beautiful_get_line() {
-	/** These are the lines for Hello Beautiful */
-	$lines = "Hello, Beautiful
-Well, hello, Beautiful
-It's so nice to see you
-You look amazing today
-You're an amazing web designer
-This site you built is gorgeous
-Have I told you lately that I love you
-Every time you log in, I smile
-How have you been beautiful
-There's a word for you, it's 'Splendiforous'
-Please come back soon, my dear
-You should win awards for the amazing work you do
-The only thing better than this website is YOU
-Other people build websites, you build works of art";
+function hello_quran_get_line() {
+	/** These are the lines for Hello quran */
+	$lines = "Hello, quran
+
+	Call upon me, I will respond to you | Surah Ghafir 40:60
+And Allah would not punish them while they seek forgiveness | Surah Al-Anfal 8:33
+So remember me; I will remember you | Surah Al-Baqarah 2:152
+And He has made me blessed wherever I am | Surah Maryam 19:31
+He knows what is within the heavens and earth and knows what you conceal and what you declare. And Allah (SWT) is Knowing of that within the breasts | At-Taghabun 64:4
+And whoever puts all his trust in Allah (SWT), He will be enough for him | Surah At-Talaq 65:1-3
+Indeed, those who have believed and done righteous deeds will have gardens beneath which rivers flow that is a great attainment | Surah al-Buruj 85:11
+If you are grateful, I will surely increase you [in favor] | Surah Ibrahim 14:7
+Between them is a barrier which they do not transgress | Surah Al-Rahman
+And He found you lost and guided [you] | Surah Ad-Duhaa 93:7
+	
+	";
 
 	// Here we split it into lines.
 	$lines = explode( "\n", $lines );
@@ -37,46 +37,46 @@ Other people build websites, you build works of art";
 }
 
 // This just echoes the chosen line, we'll position it later.
-function hello_beautiful() {
-	$chosen = hello_beautiful_get_line();
+function hello_quran() {
+	$chosen = hello_quran_get_line();
 	$lang   = '';
 	if ( 'en_' !== substr( get_user_locale(), 0, 3 ) ) {
 		$lang = ' lang="en"';
 	}
 
 	printf(
-		'<p id="beautiful"><span class="screen-reader-text">%s </span><span dir="ltr"%s>%s</span></p>',
-		__( 'Daily affirmation:', 'hello-beautiful' ),
+		'<p id="quran"><span class="screen-reader-text">%s </span><span dir="ltr"%s>%s</span></p>',
+		__( 'Daily affirmation:', 'hello-quran' ),
 		$lang,
 		$chosen
 	);
 }
 
 // Now we set that function up to execute when the admin_notices action is called.
-add_action( 'admin_notices', 'hello_beautiful' );
+add_action( 'admin_notices', 'hello_quran' );
 
 // We need some CSS to position the paragraph.
-function beautiful_css() {
+function quran_css() {
 	echo "
 	<style type='text/css'>
-	#beautiful {
+	#quran {
 		float: right;
 		padding: 5px 10px;
 		margin: 0;
-		font-size: 12px;
+		font-size: 13px;
 		line-height: 1.6666;
 		color: #d328ae;
 		font-weight: bold;
 	}
-	.rtl #beautiful {
+	.rtl #quran {
 		float: left;
 	}
-	.block-editor-page #beautiful {
+	.block-editor-page #quran {
 		display: none;
 	}
 	@media screen and (max-width: 782px) {
-		#beautiful,
-		.rtl #beautiful {
+		#quran,
+		.rtl #quran {
 			float: none;
 			padding-left: 0;
 			padding-right: 0;
@@ -86,4 +86,4 @@ function beautiful_css() {
 	";
 }
 
-add_action( 'admin_head', 'beautiful_css' );
+add_action( 'admin_head', 'quran_css' );
